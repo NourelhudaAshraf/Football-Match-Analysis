f849539574988d66875e71c9c4501e6fdb3fa1ae
Nour Ashraf <108434786+NourelhudaAshraf@users.noreply.github.com>
2025-02-03 23:55:17 +0200

    Update runBackend.py

--- backend/runBackend.py
+++ backend/runBackend.py
@@ -11,7 +11,7 @@ from pathlib import Path
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Update with the origin of your frontend app
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
